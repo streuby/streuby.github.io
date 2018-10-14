72625654ce2dccf642b89c6baad4266b56d5ee4b
Stephen Amade Reuben <streuby@live.com>
2018-10-14 16:45:25 +0100

    Fix Undifined output on DataTable

--- application/application.js
+++ application/application.js
@@ -50,10 +50,10 @@ recommendations.limitToLast(2).on('child_added', function(childSnapshot) {
         recommendation = data.val();
 
         $('#myTableBody').append('<tr>'
-        +'<td>'+recommendation.title+'</td>'
-        +'<td>'+recommendation.presenter+'</td>'
-        +'<td>'+recommendation.version+'</td>'
-        +'<td>'+recommendation.link+'</td>'+'</tr>')
+        +'<td>'+recommendation.title()+'</td>'
+        +'<td>'+recommendation.presenter()+'</td>'
+        +'<td>'+recommendation.version()+'</td>'
+        +'<td>'+recommendation.link()+'</td>'+'</tr>')
 
         console.log(recommendation.title)
     
