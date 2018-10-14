c5837d7c2a2c259c5389b076820ca91fb50fda03
Stephen Amade Reuben <streuby@live.com>
2018-10-14 07:52:23 +0100

    Cange html content

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function WriteToFile(passForm) {
  
-    set fso = CreateObject("Scripting.FileSystemObject"); 
-    set s   = fso.CreateTextFile("C:\Users\Sarb-PC\streuby.github.io/filename.txt", True);
+    var fso = CreateObject("Scripting.FileSystemObject"); 
+    var s   = fso.CreateTextFile("C:\Users\Sarb-PC\streuby.github.io/db.txt", True);
  
     var firstName = document.getElementById('FirstName');
     var lastName  = document.getElementById('lastName');
