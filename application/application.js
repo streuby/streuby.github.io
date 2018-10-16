a4223eeee00ff621a187bf534aea0ec2457c06a4
Stephen Amade Reuben <streuby@live.com>
2018-10-16 15:07:16 +0100

    fix

--- application/application.js
+++ application/application.js
@@ -50,7 +50,7 @@ recommendations.limitToLast(5).on('child_added', function(childSnapshot) {
         
     // });
     for (i = 0; i < childSnapshot.numChildren(); i++) {
-    console.log(childSnapshot[i].apiKey);
+    console.log(childSnapshot[i].Key);
     }
 
     // childSnapshot.forEach(function(data) {
